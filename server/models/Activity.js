f24667013d40d7c74ffb254fee6b3ce0ba2e093b
Braden Walker <bradencodes@gmail.com>
2019-08-30 14:46:19 -0700

    adjust Activity schema

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -1,4 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ActivitySchema = new Schema({});
+const ActivitySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  progress: {
+    type: Number,
+    required: true
+  },
+  target: {
+    type: Number,
+    required: true
+  },
+  start: {
+    type: Map,
+    of: [[Number]],
+    required: true
+  },
+  end: {
+    type: Map,
+    of: [[Number]],
+    required: true
+  },
+  repeat: {
+    type: [Boolean],
+    required: true
+  },
+  adds: {
+    type: Boolean,
+    required: true
+  },
+  nextReset: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = Activity = mongoose.model('activity', ActivitySchema);
