91e0449bc55f9e9d8acae4e4e538ffb9a8f7eedc
Braden Walker <bradencodes@gmail.com>
2019-09-04 10:31:58 -0700

    add reference to User in Week and Activity Schemas

--- server/models/Week.js
+++ server/models/Week.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WeekSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
   start: {
     type: Number,
     required: true
