a84424677d79d2a676f5525caad686625bf92714
Braden Walker <bradencodes@gmail.com>
2019-09-07 18:03:12 -0700

    collapsible app bar refactored to use arrow functions and hooks

--- client/src/components/dashboard/CollapsibleAppBar.js
+++ client/src/components/dashboard/CollapsibleAppBar.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
-const styles = {
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
   },
   appBar: {
-    zIndex: 10
+    zIndex: theme.zIndex.drawer + 1
   },
   menuButton: {
     marginRight: 36
@@ -22,102 +25,79 @@ const styles = {
   },
   show: {
     transform: 'translate(0, 0)',
-    transition: 'transform .25s',
+    transition: 'transform .25s'
   },
   hide: {
     transform: 'translate(0, -70px)',
-    transition: 'transform .25s',
-  },
-};
-
-class CollapsibleAppBar extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      shouldShow: null,
-    };
-
-    this.lastScroll = null;
-
-    this.handleScroll = this.handleScroll.bind(this);
-    // Alternatively, you can throttle scroll events to avoid
-    // updating the state too often. Here using lodash.
-    // this.handleScroll = _.throttle(this.handleScroll.bind(this), 100);
+    transition: 'transform .25s'
   }
+}));
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll, { passive: true });
-  }
+const CollapsibleAppBar = props => {
+  const classes = useStyles();
+  const [show, changeShow] = React.useState(null);
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
+  let lastScroll = null;
 
-  handleScroll() {
-    const lastScroll = window.scrollY;
+  const handleScroll = () => {
+    const windowLastScroll = window.scrollY;
 
-    if (lastScroll === this.state.lastScroll) {
+    if (windowLastScroll === lastScroll) {
       return;
     }
 
     const shouldShow =
-      this.lastScroll !== null ? lastScroll < this.lastScroll : null;
+      lastScroll !== null ? windowLastScroll < lastScroll : null;
 
-    if (shouldShow !== this.state.shouldShow) {
-      this.setState(prevState => ({
-        ...prevState,
-        shouldShow,
-      }));
+    if (shouldShow !== show) {
+      changeShow(shouldShow);
     }
 
-    this.lastScroll = lastScroll;
-  }
+    lastScroll = windowLastScroll;
+  };
 
-  getScrollClassName() {
-    if (this.state.shouldShow === null) {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const getScrollClassName = () => {
+    if (show === null) {
       return '';
     }
 
-    return this.state.shouldShow
-      ? this.props.classes.show
-      : this.props.classes.hide;
-  }
+    return show ? classes.show : classes.hide;
+  };
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <AppBar
-        position="fixed"
-        className={`${classes.root} ${this.getScrollClassName()} ${classes.appBar}`}
-      >
-        <Toolbar className={classes.toolbar}>
-            <IconButton
-              edge='start'
-              color='inherit'
-              aria-label='open drawer'
-              onClick={this.props.handleDrawerOpen}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component='h1'
-              variant='h6'
-              color='inherit'
-              noWrap
-              className={classes.title}
-            >
-              Cloxel
-            </Typography>
-          </Toolbar>
-      </AppBar>
-    );
-  }
-}
-
-CollapsibleAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+  return (
+    <AppBar
+      position='fixed'
+      className={`${classes.root} ${getScrollClassName()} ${classes.appBar}`}
+    >
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          edge='start'
+          color='inherit'
+          aria-label='open drawer'
+          onClick={props.handleDrawerOpen}
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component='h1'
+          variant='h6'
+          color='inherit'
+          noWrap
+          className={classes.title}
+        >
+          Cloxel
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-export default withStyles(styles)(CollapsibleAppBar);
+CollapsibleAppBar.propTypes = {};
+
+export default CollapsibleAppBar;
