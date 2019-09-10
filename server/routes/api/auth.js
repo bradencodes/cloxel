1325700afa6ac8b2e666fe2d29ae624ea998e174
Braden Walker <bradencodes@gmail.com>
2019-09-10 09:38:10 -0700

    populate activity fields when GETting user

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -12,7 +12,11 @@ const User = require('../../models/User');
 // @access  Private
 router.get('/', auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id)
+      .select('-password')
+      .populate('activities')
+      .populate('active')
+      .populate('breaktime');
     res.json(user);
   } catch (err) {
     console.error(err.message);
