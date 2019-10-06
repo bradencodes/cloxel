86c23a778040d22b641cd5c13d488242c5319dbc
Braden Walker <bradencodes@gmail.com>
2019-10-06 14:38:12 -0700

    make changeDoing a put request from breaktimeCard

--- client/src/components/activities/BreaktimeCard.js
+++ client/src/components/activities/BreaktimeCard.js
@@ -112,7 +112,7 @@ const BreaktimeCard = ({
     try {
       dispatch(changeDoing(user, breaktime._id, user.active, now));
 
-      await axios.post(`${urlpre}/api/activities/changeDoing`, body, config);
+      await axios.put(`${urlpre}/api/activities/changeDoing`, body, config);
 
       socket.emit('change doing', breaktime._id, user.active, now);
     } catch (err) {
